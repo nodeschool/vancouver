b19d729de9be72ba47b7c7f9a3206a0240e87dac
Boris Mann <boris@bmannconsulting.com>
2015-05-24 10:11:45 -0700

    Jekyll-ized a bunch of things to make it easy for other groups to re-use

--- js/app.js
+++ js/app.js
@@ -10,12 +10,12 @@ function getRecentMeet(response) {
         nextEventId = nextEvent.id;
         nextEventName = nextEvent.name;
         nextEventDiv = document.querySelector(".next-event");
-        nextEventDiv.innerHTML = "<a href='http://www.meetup.com/atxnodejs/events/" + nextEventId + "'>" + nextEventName + "</a>";
+        nextEventDiv.innerHTML = "<a href='{{http://www.meetup.com/{{site.meetup}}/events/}}" + nextEventId + "'>" + nextEventName + "</a>";
     }
 }
 
 (function() {
     var script = document.createElement("script");
-    script.src = "https://api.meetup.com/atxnodejs?&sign=true&page=5&api&key=5049797817560b4e5c4594387922&callback=getRecentMeet";
+    script.src = "https://api.meetup.com/{{site.meetup}}?&sign=true&page=5&api&key=5049797817560b4e5c4594387922&callback=getRecentMeet";
     document.body.appendChild(script);
 }());
