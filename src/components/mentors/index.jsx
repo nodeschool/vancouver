08297ad249636aa0014f42fed6f7ccb2f96f5c10
Gavin Mogan <git@gavinmogan.com>
2019-10-27 00:10:44 -0700

    cleanup

--- src/components/mentors/index.jsx
+++ src/components/mentors/index.jsx
@@ -1,39 +1,6 @@
 import React from "react";
 import { useStaticQuery, graphql } from "gatsby";
 import PeopleHexagons from '../people-hexagons';
-import styled from "@emotion/styled";
-import Hexagon from 'react-hexagon';
-
-const MentorContainer = styled.div`
-  display: grid;
-  grid-gap: 0px;
-  grid-template-columns: repeat(auto-fit, minmax(120px, 0.25fr));
-
-  width: 100%;
-  z-index: 900;
-  transform: translate3d(-5%, 0, 0);
-  position: relative;
-
-  &:hover {
-    svg {
-      transition: opacity 0.3s, transform 0.2s;
-      opacity: 0.25;
-      &:hover {
-        opacity: 1 !important;
-        transform: scale(1.5);
-        z-index: 9999;
-      }
-    }
-  }
-  svg {
-    padding: 0;
-    margin: 0;
-    polygon {
-      stroke: transparent !important;
-      stroke-width: 0 !important;
-    }
-  }
-`;
 
 export const Mentors = () => {
   const data = useStaticQuery(graphql`
@@ -61,20 +28,7 @@ export const Mentors = () => {
       {/* mentors */}
       <PeopleHexagons people={data.allMentorsYaml.edges.map(edge => edge.node)} />
       <p>
-        If you want to help teach or contribute in anyway, make a
-        <strong>
-          <a href={`https://github.com/${data.site.siteMetadata.github}/pulls`}>
-          Pull Request
-          </a>
-        </strong>
-        and add your name to the list of available mentors. We also ask
-        that you please read the
-        <a href="https://github.com/nodeschool/organizers/wiki/Event-Mentor-Best-Practices#on-being-a-mentor">
-          &quot;On Being a Mentor&quot;
-        </a>
-        tips for <strong>NodeSchool Organizers</strong> before attending as a mentor.
-      </p>
-
+        If you want to help teach or contribute in anyway, make a <strong><a href={`https://github.com/${data.site.siteMetadata.github}/pulls`}>Pull Request</a></strong> and add your name to the list of available mentors. We also ask that you please read the <a href="https://github.com/nodeschool/organizers/wiki/Event-Mentor-Best-Practices#on-being-a-mentor">&quot;On Being a Mentor&quot;</a> tips for <strong>NodeSchool Organizers</strong> before attending as a mentor.  </p>
         {/*
         <p>
           Mentors are currently <strong>contacted and confirmed</strong> before each event in our
