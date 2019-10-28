6bfd88f54083f60a8bfa8457ed78f2b313fe44b3
Gavin Mogan <git@gavinmogan.com>
2019-10-27 17:02:31 -0700

    Yay google maps and how to find us, and fix tabs

--- src/components/events/upcoming_events.js
+++ src/components/events/upcoming_events.js
@@ -39,28 +39,29 @@ export const UpcomingEvents = () => {
     }
   `);
   return data.allMeetupEvent.edges.map(edge => {
-		const address = [
-			edge.node.venue.name,
-			edge.node.venue.address_1,
-			edge.node.venue.address_2,
-			[edge.node.venue.city,edge.node.venue.state].filter(Boolean).join(`, `),
-			edge.node.venue.zip,
-			edge.node.venue.localized_country_name,
-		].filter(Boolean);
+    const address = [
+      edge.node.venue.name,
+      edge.node.venue.address_1,
+      edge.node.venue.address_2,
+      [edge.node.venue.city,edge.node.venue.state].filter(Boolean).join(`, `),
+      edge.node.venue.zip,
+      edge.node.venue.localized_country_name,
+    ].filter(Boolean);
     return (
       <>
         <p className="register">
-          <strong>Register</strong> for our event on <strong>{(new Date(edge.node.time)).toLocaleDateString()}</strong>
+          <strong><a href={edge.node.link} title="Registration Link">Register</a></strong> for our event on <strong>{(new Date(edge.node.time)).toLocaleDateString()}</strong>
         </p>
         <p className="location">
           <strong>Location:</strong>&nbsp;
            <address>{address.map(line => <span key={line}>{line}<br/></span>)}</address>
+           {edge.node.how_to_find_us}
         </p>
         <div className="map">
 
           <iframe
             title="google maps"
-						src={`https://maps.google.com/maps?q=${encodeURIComponent(address.join(`\n`))}&t=&z=13&ie=UTF8&iwloc=&output=embed`}
+            src={`https://maps.google.com/maps?q=${encodeURIComponent(address.join(`\n`))}&t=&z=13&ie=UTF8&iwloc=&output=embed`}
             width="600"
             height="450"
             frameBorder="0"
